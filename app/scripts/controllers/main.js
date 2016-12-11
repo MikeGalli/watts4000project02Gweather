b211cdc415a9daefb4eb38f7939dfc44d9961e69
Mike G <mkgalligar@gmail.com>
2016-12-10 17:01:18 -0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name watts4000project02GweatherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the watts4000project02GweatherApp
+ */
+angular.module('watts4000project02GweatherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
